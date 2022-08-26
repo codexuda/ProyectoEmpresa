2c950e664083d75a21f23236bcacb386d0956647
MMartinezLopez <mauromarto07@gmail.com>
2022-08-26 18:23:58 -0500

    Creacion del proyecto en SpringBoot

---
+++ src/main/java/empresa/proyectoempresa/ProyectoempresaApplication.java
@@ -0,0 +1,13 @@
+package empresa.proyectoempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoempresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoempresaApplication.class, args);
+	}
+
+}
