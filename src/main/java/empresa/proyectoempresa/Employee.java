4307a13522ff5e45bbb91d45514050d2726e9378
MMartinezLopez <mauromarto07@gmail.com>
2022-08-26 22:44:44 -0500

    Implementacion atributos clase empleado

--- src/main/java/empresa/proyectoempresa/Employee.java
+++ src/main/java/empresa/proyectoempresa/Employee.java
@@ -1,4 +1,56 @@
 package empresa.proyectoempresa;
 public class Employee {
+
+    //Atributos
+    private String nombre, rol, correo;
+    private int nitEmpresa, idEmpleado;
+    
+    public Employee(String nombre, String correo, String rol, int id, int nit){
+        this.nombre=nombre;
+        this.correo=correo;
+        this.idEmpleado=id;
+        this.nitEmpresa=nit;
+        this.rol=rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public int getNit() {
+        return nitEmpresa;
+    }
+
+    public int getId() {
+        return idEmpleado;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setNit(int nit) {
+        this.nitEmpresa = nit;
+    }
+
+    public void setId(int id) {
+        this.idEmpleado = id;
+    }
     
 }
