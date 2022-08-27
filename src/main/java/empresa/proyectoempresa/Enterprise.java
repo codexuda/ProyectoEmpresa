2b970153ef275b30763c8409d42710900a9689a2
Arnold Alvarez <arnoldmauricio15@gmail.com>
2022-08-26 22:32:42 -0500

    implementacion de atributos clase empresa

--- src/main/java/empresa/proyectoempresa/Enterprise.java
+++ src/main/java/empresa/proyectoempresa/Enterprise.java
@@ -1,4 +1,51 @@
 package empresa.proyectoempresa;
 public class Enterprise {
-    
+
+    //Atributos
+    private String name,document,phone,adress;
+    private long id;
+
+      
+    //getters and setters
+    public String getName() {
+        return name;
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDocument() {
+        return document;
+    }
+    public void setDocument(String document) {
+        this.document = document;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getAdress() {
+        return adress;
+    }
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+     //Constructor
+     public Enterprise(String name, String document, String phone, String adress, long id) {
+        this.name = name;
+        this.document = document;
+        this.phone = phone;
+        this.adress = adress;
+        this.id = id;
+    }
+
 }
