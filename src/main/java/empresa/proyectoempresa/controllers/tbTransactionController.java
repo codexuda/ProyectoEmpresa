6fbf61a21d493c9bdf76b85594daf76fc2596130
jasondarkside <jasondarkside@me.com>
2022-08-31 18:22:06 -0500

    se imprementa el controlador de la entidad transaccion

--- src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
+++ src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
@@ -1,11 +1,11 @@
 package empresa.proyectoempresa.controllers;
 
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import empresa.proyectoempresa.repositories.tbTransactionRepository;
-
+import  empresa.proyectoempresa.modelo.*;
 
 @RestController
 @RequestMapping ("/transaction")
@@ -13,5 +13,10 @@ public class tbTransactionController {
 
     @Autowired
     private tbTransactionRepository repositiorio;
+
+public List<tbTransaction> listarTransaccion (){
+    return repositiorio.findAll();
+    
+}
 
 }
