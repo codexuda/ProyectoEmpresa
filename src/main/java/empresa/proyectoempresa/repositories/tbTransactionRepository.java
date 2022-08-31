49b23ef2fcfebc69e42d2dde72c23356c31b0e38
jasondarkside <jasondarkside@me.com>
2022-08-31 09:06:54 -0500

    se crea repository de transaction

--- src/main/java/empresa/proyectoempresa/repositories/tbTransactionRepository.java
+++ src/main/java/empresa/proyectoempresa/repositories/tbTransactionRepository.java
@@ -3,7 +3,9 @@ package empresa.proyectoempresa.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import empresa.proyectoempresa.modelo.tbTransaction;
+
 @Repository
-public interface tbTransactionRepository extends JpaRepository <tbTransactionRepository,Long>{
+public interface tbTransactionRepository extends JpaRepository <tbTransaction,Long>{
     
 }
