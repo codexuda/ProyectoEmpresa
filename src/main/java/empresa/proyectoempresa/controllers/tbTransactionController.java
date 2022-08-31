dc1be9f24b7ab95143ba301ff0ebbd723296f4b7
jasondarkside <jasondarkside@me.com>
2022-08-31 18:54:07 -0500

    se imprementa el controlador de la entidad transaccion se agrega metodo GET para consultar la trasaccion

--- src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
+++ src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
@@ -1,23 +1,22 @@
 package empresa.proyectoempresa.controllers;
 
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import empresa.proyectoempresa.repositories.tbTransactionRepository;
-import  empresa.proyectoempresa.modelo.*;
+import empresa.proyectoempresa.modelo.*;
 
 @RestController
-@RequestMapping ("/transaction")
+@RequestMapping("/transaction")
 public class tbTransactionController {
 
     @Autowired
     private tbTransactionRepository repositiorio;
 
-    @RequestMapping(value ="/consultartransaccion", method= RequestMethod.GET )
-    public List<tbTransaction> listarTransaccion (){
+    @RequestMapping(value = "/consultartransaccion", method = RequestMethod.GET)
+    public List<tbTransaction> listarTransaccion() {
         return repositiorio.findAll();
-    
-}
+
+    }
 
 }
