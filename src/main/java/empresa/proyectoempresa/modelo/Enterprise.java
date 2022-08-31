54e82caab8b3d5bf2a9fb5452ff923a3150b1f54
MMartinezLopez <mauromarto07@gmail.com>
2022-08-30 21:21:32 -0500

    Actualización al modelo Enterprise (Empresa)

--- src/main/java/empresa/proyectoempresa/modelo/Enterprise.java
+++ src/main/java/empresa/proyectoempresa/modelo/Enterprise.java
@@ -1,45 +1,63 @@
 package empresa.proyectoempresa.modelo;
 
 
+import java.sql.Date;
+
 import javax.persistence.*;
 @Entity
 public class Enterprise  {
-    private long phone;
-    @Column(name="createdate", nullable=true)
-    private String createdate;
-    @Column(name="updatedate", nullable=true)
-    private String updatedate;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ident;
+
     @Column(name="nameenterprise",  length=100)
     private String nameenterprise;
+
     @Column(name="address", nullable=true, length=150)
     private String address;
-    
-    public Enterprise () {}
 
+    @Column(name = "phone")
+    private long phone;
+
+    @Column(name="createdate", nullable=true)
+    private Date createdate;
+
+    @Column(name="updatedate", nullable=true)
+    private Date updatedate;
+
+    //Constructor
+    public Enterprise(long ident, String nameenterprise, String address, long phone, Date createdate, Date updatedate) {
+        this.ident = ident;
+        this.nameenterprise = nameenterprise;
+        this.address = address;
+        this.phone = phone;
+        this.createdate = createdate;
+        this.updatedate = updatedate;
+    }
+
+    // Getters & Setters
     public long getPhone() {
         return phone;
     }
+    
     public void setPhone(long phone) {
         this.phone = phone;
     }
 
-
-    public String getcreatedate() {
+    public Date getcreatedate() {
         return createdate;
     }
-    public void setcreatedate(String createdate) {
+    public void setcreatedate(Date createdate) {
         this.createdate = createdate;
     }
 
-
-    public String getupdatedate() {
+    public Date getupdatedate() {
         return updatedate;
     }
-    public void setupdatedate(String updatedate) {
+    public void setupdatedate(Date updatedate) {
         this.updatedate = updatedate;
     }
-
 
     public long getIdent() {
         return ident;
@@ -48,7 +66,6 @@ public class Enterprise  {
         this.ident = ident;
     }
 
-
     public String getNameenterprise() {
         return nameenterprise;
     }
@@ -56,16 +73,14 @@ public class Enterprise  {
         this.nameenterprise = nameenterprise;
     }
 
-
     public String getAddress() {
         return address;
     }
     public void setAddress(String address) {
         this.address = address;
     }
-
-
 }
+
 
 
 
