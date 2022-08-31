b92ea6b16036d492aa8d9d1097fb38f02188329b
jasondarkside <jasondarkside@me.com>
2022-08-31 11:58:17 -0500

    se empieza a crear los controllers

---
+++ src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
@@ -0,0 +1,11 @@
+package empresa.proyectoempresa.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping ("/transaction")
+public class tbTransactionController {
+    
+}
