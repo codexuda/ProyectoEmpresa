58f33e226cbf98ad02fd1cc16254de6e0e8744b0
Arnold Alvarez <arnoldmauricio15@gmail.com>
2022-09-01 00:31:55 -0500

    creacion de contructor y modelos clase profile

--- src/main/java/empresa/proyectoempresa/modelo/Profile.java
+++ src/main/java/empresa/proyectoempresa/modelo/Profile.java
@@ -1,68 +1,79 @@
 package empresa.proyectoempresa.modelo;
 
 import javax.persistence.*;
+
+
 @Entity
+@Table(name="profile")
 public class Profile {
     @Id
-    private long idprof;
+     private long idprof;
     private long phone;
-    @Column(name="createdate", nullable=true)
+    @Column(name = "createdate", nullable = true)
     private String createdate;
-    @Column(name="updatedate", nullable=true)
+    @Column(name = "updatedate", nullable = true)
     private String updatedate;
-    @Column(name="image", nullable=true, length=100)
+    @Column(name = "image", nullable = true, length = 100)
     private String image;
-    @Column(name="userp", nullable=true, length=100)
+    @Column(name = "userp", nullable = true, length = 100)
     private String userp;
-    
-    public Profile() {}
+
+    public Profile(long idprof, long phone, String createdate, String updatedate, String image, String userp) {
+        this.idprof = idprof;
+        this.phone = phone;
+        this.createdate = createdate;
+        this.updatedate = updatedate;
+        this.image = image;
+        this.userp = userp;
+
+    }
 
     public long getIdprof() {
         return idprof;
     }
+
     public void setIdprof(long idprof) {
         this.idprof = idprof;
     }
 
-
     public long getPhone() {
         return phone;
     }
+
     public void setPhone(long phone) {
         this.phone = phone;
     }
 
-
     public String getcreatedate() {
         return createdate;
     }
+
     public void setcreatedate(String createdate) {
         this.createdate = createdate;
     }
 
-
     public String getupdatedate() {
         return updatedate;
     }
+
     public void setupdatedate(String updatedate) {
         this.updatedate = updatedate;
     }
 
-
     public String getImage() {
         return image;
     }
+
     public void setImage(String image) {
         this.image = image;
     }
 
-
     public String getUserp() {
         return userp;
     }
+
     public void setUserp(String userp) {
         this.userp = userp;
     }
-
 
 }
