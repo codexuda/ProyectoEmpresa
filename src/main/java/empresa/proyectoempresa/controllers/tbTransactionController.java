2f97ba48c457e960e040ae0bf3603d29063b49a8
jasondarkside <jasondarkside@me.com>
2022-09-02 20:02:52 -0500

    se imprmenta masmetodos al controlador de trsaccion

--- src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
+++ src/main/java/empresa/proyectoempresa/controllers/tbTransactionController.java
@@ -7,16 +7,31 @@ import empresa.proyectoempresa.repositories.tbTransactionRepository;
 import empresa.proyectoempresa.modelo.*;
 
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/transactions")
 public class tbTransactionController {
 
     @Autowired
     private tbTransactionRepository repositiorio;
 
-    @RequestMapping(value = "/consultartransaccion", method = RequestMethod.GET)
+    //Listar todas las transacciones----GET
+    @RequestMapping(value = "/listartransacciones", method = RequestMethod.GET)
     public List<tbTransaction> listarTransaccion() {
         return repositiorio.findAll();
 
     }
+    //Consultar un transaccion por ID---"PATCH"
+    @RequestMapping(value = "/buscartransaccion/{idTran}", method = RequestMethod.GET)
+    public tbTransaction buscartransaccion(@pathVariable long idTran) {
+        return repositories.findById(idTran).get();
+    }
+
+    //Agregar transaccion-----POST
+    @RequestMapping (value = "/add",method = RequestMethod.POST)
+     public tbTransaction create(tbTransaction tbtransaction){  
+        return repositories.save(tbtransaction) ;
+    }
+
+
+    
 
 }
