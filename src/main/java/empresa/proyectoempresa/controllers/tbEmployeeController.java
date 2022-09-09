b1ed15873291a37717d9cfdfa6d0e6bf76ce6dfc
Jonathan Bernal <jonathanbpf@yahoo.es>
2022-09-09 16:19:48 -0500

    Cambiar forma de llamar los metodos en controlador de employye, cambio en el modelo employe para agregar la fecha automaticamente

--- src/main/java/empresa/proyectoempresa/controllers/tbEmployeeController.java
+++ src/main/java/empresa/proyectoempresa/controllers/tbEmployeeController.java
@@ -1,8 +1,12 @@
 package empresa.proyectoempresa.controllers;
 
+import java.lang.reflect.Field;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 import empresa.proyectoempresa.modelo.tbEmployee;
@@ -15,41 +19,49 @@ public class tbEmployeeController {
     private tbEmployeeRepository repository;
 
     // metodo listar toda la tabla
-    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @GetMapping(value = "")
     public List<tbEmployee> listar() {
         return repository.findAll();
     }
 
     // metodo listar un elemento por id
-    @RequestMapping(value = "/get/{idemp}", method = RequestMethod.GET)
+    @GetMapping(value = "/{idemp}")
     public tbEmployee obtener(@PathVariable long idemp) {
         return repository.findById(idemp).get();
     }
 
     // metodo crear un registro
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public tbEmployee crear(@RequestBody tbEmployee employee) {
+    @PostMapping(value = "/add")
+    public tbEmployee agregar(@RequestBody tbEmployee employee) {
+        employee.setCreated(LocalDate.now());
+        employee.setUpdated(LocalDate.now());
         return repository.save(employee);
-
     }
 
-    // metodo editar un registro
-    //@RequestMapping(value = "/modificar", method = RequestMethod.PUT)
-    //public tbEmployee actualizar(@RequestBody tbEmployee employee) {
-    //    return repository.save(employee);
-    //}
+    // metodo editar un registro con el metodo PUT
+    // @RequestMapping(value = "/modificar", method = RequestMethod.PUT)
+    // public tbEmployee actualizar(@RequestBody tbEmployee employee) {
+    // return repository.save(employee);
+    // }
 
-    //metodo para editar registros
-    @RequestMapping(value = "/update/{idemp}", method =  RequestMethod.PATCH)
-    public tbEmployee actualizar(@RequestBody tbEmployee employee){
+    // metodo para editar registros con metodo PATCH
+    @PatchMapping(value = "/{idemp}/update")
+    public tbEmployee actualizar(@PathVariable long idemp, @RequestBody Map<Object, Object> fields){
+        tbEmployee employee=repository.findById(idemp).get();
+        fields.forEach((y,h) ->{
+            Field field= ReflectionUtils.findField(clazz:tbEmployee.class,(String) y);
+            field.setAccessible(true);
+            ReflectionUtils.setField(field, employee, h);
+        });
+        employee.setUpdated(LocalDate.now());
         return repository.save(employee);
     }
 
     // metodo eliminar un registro
-    @RequestMapping(value = "/delete/{idemp}", method = RequestMethod.DELETE)
-    public String eliminarEmpleado(@PathVariable long idemp) {
-        tbEmployee employee=repository.findById(idemp).get();
-        repository.delete(employee);
-        return "Eliminado con éxito";
+    @DeleteMapping(value = "/{idemp}/delete")
+    public String eliminar(@PathVariable long idemp) {
+        repository.delete(repository.findById(idemp).get());
+        return "Eliminada con éxito";
     }
+
 }
