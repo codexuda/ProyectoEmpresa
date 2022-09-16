ea2eb3b9a9136572eb56274a11cbcf64bb033cc9
Jonathan Bernal <jonathanbpf@yahoo.es>
2022-09-15 21:25:14 -0500

    se carga el servicio web

--- src/main/java/empresa/proyectoempresa/controllers/FrontController.java
+++ src/main/java/empresa/proyectoempresa/controllers/FrontController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+//mport empresa.proyectoempresa.modelo.*;
 import empresa.proyectoempresa.services.tbEmployeeService;
 
 @Controller
-@RequestMapping("/Employee")
+@RequestMapping("/")
     public class FrontController{
         @Autowired
-        private tbEmployeeService tbEmployeeService;
+        tbEmployeeService tbEmployeeService;
 
         @RequestMapping(value = "/", method = RequestMethod.GET)
         public String index(){
