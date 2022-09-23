3113c6404d3f8f399feeb192090cf8f29b0cc1ed
MMartinezLopez <mauromarto07@gmail.com>
2022-09-23 00:00:54 -0500

    Css a index

--- src/main/java/empresa/proyectoempresa/controllers/FrontController.java
+++ src/main/java/empresa/proyectoempresa/controllers/FrontController.java
@@ -5,21 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
 import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> f221577c7b116c1aaf5de7bcf437ac7516c729da
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import empresa.proyectoempresa.modelo.*;
-import empresa.proyectoempresa.modelo.tbTransaction;
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import empresa.proyectoempresa.modelo.*;
->>>>>>> 69a03643977512ca546314d40b7208b7c057d53e
+
 import empresa.proyectoempresa.services.*;
 import empresa.proyectoempresa.controllers.tbEmployeeController;
 
@@ -27,15 +19,18 @@ import empresa.proyectoempresa.controllers.tbEmployeeController;
 @RequestMapping("/")
 public class FrontController {
 
-    @Autowired
-    tbTransactionService tbtransactionService;
-
+    //Index
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "index";
     }
 
-    // Controlador que conecta con la View
+
+    // Controlador View Transacciones
+
+    @Autowired
+    tbTransactionService tbtransactionService;
+
     @RequestMapping(value = "/movimientos", method = RequestMethod.GET)
     public String ViewlistTransaccion(Model model) {
         List<tbTransaction> ViewlistTransaccion = this.tbtransactionService.listarTransaccion();
@@ -43,16 +38,19 @@ public class FrontController {
         return "ListTransactions";
     }
 
-<<<<<<< HEAD
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add(@ModelAttribute tbTransaction tbtransaction, Model model){
         System.out.println(tbtransaction);
         model.addAttribute("tbtransaction", new tbTransaction());
         return "AddTransactions";
-=======
+    }
+
+
+
     // Bloque para listar empleado
+    /*
     @Autowired
-    tbEmployeeService tbEmployeeService;
+    tbEmployeeService employeeService;
 
     // metodo para listar empleados en el front end, llamando el metodo desde el
     // controlador
@@ -61,16 +59,14 @@ public class FrontController {
         List<tbEmployee> listEmployee = this.tbEmployeeService.getAllEmployees();
         model.addAttribute("listEmployee", listEmployee);
         return "ListEmployee";
->>>>>>> 69a03643977512ca546314d40b7208b7c057d53e
     }
     // fin bloque listar empleado
 
-<<<<<<< HEAD
     //Bloque para agregar empleado
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add(@ModelAttribute tbEmployee employee, Model model){
         model.addAttribute("employee", new tbEmployee());
-        return "AddEmployee";
+        return "addEmployee";
 
     }
 
@@ -81,54 +77,28 @@ public class FrontController {
     }
     //fin bloque agregar empleado//
 
-    //fin bloque agregar empleado
 
-=======
-<<<<<<< HEAD
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addTransaction(@ModelAttribute tbTransaction tbtransaction, Model model){
+        System.out.println(tbtransaction);
+        this.tbtransactionService.createTransaction(tbtransaction);
+        return "redirect:/movimientos";
 
-        @RequestMapping(value = "/add", method = RequestMethod.POST)
-        public String addTransaction(@ModelAttribute tbTransaction tbtransaction, Model model){
-            System.out.println(tbtransaction);
-            this.tbtransactionService.createTransaction(tbtransaction);
-            return "redirect:/movimientos";
-
-        }
+    }
+    */
         
-    //Bloque para listar empleado
-    //@Controller
-    //@RequestMapping("/")
-    //public class FrontController {
-    //    @Autowired
-       // tbEmployeeService tbEmployeeService;
-        //@RequestMapping(value = "/", method = RequestMethod.GET)
-        //public String index() {
-            //return "index";
-        //}
-        // metodo para listar empleados en el front end, llamando el metodo desde el controlador
-      //  @RequestMapping(value = "/employee", method = RequestMethod.GET)
-       // public String listEmployee(Model model) {
-         //   List<tbEmployee> listEmployee = this.tbEmployeeService.getAllEmployees();
-           // model.addAttribute("listEmployee", listEmployee);
-            //return "Employee";
-       // }
 
-    //fin bloque listar empleado
-=======
->>>>>>> 69a03643977512ca546314d40b7208b7c057d53e
->>>>>>> f221577c7b116c1aaf5de7bcf437ac7516c729da
+    
+
+    // Controlador Vista Empresa
     @Autowired
     tbEnterpriseService enterpriseService;
 
-    // Controlador Vista Empresa
     @RequestMapping(value = "/empresas", method = RequestMethod.GET)
     public String ViewListEnterprises(Model model) {
         List<tbEnterprise> ViewListEnterprises = this.enterpriseService.listarEmpresas();
         model.addAttribute("ViewListEnterprises", ViewListEnterprises);
         return "ListEnterprise";
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> 69a03643977512ca546314d40b7208b7c057d53e
     }
 }
