083b8d7dec3f0811daaac5c21b14849eb8300942
Jonathan Bernal <jonathanbpf@yahoo.es>
2022-09-22 19:46:43 -0500

    depurar librerias y comentarios en FrontController

--- src/main/java/empresa/proyectoempresa/controllers/FrontController.java
+++ src/main/java/empresa/proyectoempresa/controllers/FrontController.java
@@ -2,18 +2,13 @@ package empresa.proyectoempresa.controllers;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import empresa.proyectoempresa.modelo.*;
-//import empresa.proyectoempresa.modelo.tbTransaction;
 import empresa.proyectoempresa.services.*;
-
-
 
 @Controller
 @RequestMapping("/")
@@ -34,50 +29,30 @@ public class FrontController {
         model.addAttribute("ViewlistTransaccion", ViewlistTransaccion);
         return "ListTransaccion";
     }
-<<<<<<< HEAD
 
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add(@ModelAttribute tbTransaction tbtransaction, Model model){
-        System.out.println(tbtransaction);
-        model.addAttribute("tbtransaction", new tbTransaction());
-        return "AgregarTransaccion";
+    // Bloque para listar empleado
+    @Autowired
+    tbEmployeeService tbEmployeeService;
+
+    // metodo para listar empleados en el front end, llamando el metodo desde el
+    // controlador
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String listEmployee(Model model) {
+        List<tbEmployee> listEmployee = this.tbEmployeeService.getAllEmployees();
+        model.addAttribute("listEmployee", listEmployee);
+        return "Employee";
     }
+    // fin bloque listar empleado
 
-
-        @RequestMapping(value = "/add", method = RequestMethod.POST)
-        public String addTransaction(@ModelAttribute tbTransaction tbtransaction, Model model){
-            System.out.println(tbtransaction);
-            this.tbtransactionService.createTransaction(tbtransaction);
-            return "redirect:/movimientos";
-=======
-    //Bloque para listar empleado
-    //@Controller
-    //@RequestMapping("/")
-    //public class FrontController {
-        @Autowired
-        tbEmployeeService tbEmployeeService;
-        //@RequestMapping(value = "/", method = RequestMethod.GET)
-        //public String index() {
-            //return "index";
-        //}
-        // metodo para listar empleados en el front end, llamando el metodo desde el controlador
-        @RequestMapping(value = "/employee", method = RequestMethod.GET)
-        public String listEmployee(Model model) {
-            List<tbEmployee> listEmployee = this.tbEmployeeService.getAllEmployees();
-            model.addAttribute("listEmployee", listEmployee);
-            return "Employee";
-        }
-
-    //fin bloque listar empleado
     @Autowired
     tbEnterpriseService enterpriseService;
-    //Controlador Vista Empresa
+
+    // Controlador Vista Empresa
     @RequestMapping(value = "/empresas", method = RequestMethod.GET)
-    public String ViewListEnterprises(Model model){
-        List<tbEnterprise> ViewListEnterprises= this.enterpriseService.listarEmpresas();
+    public String ViewListEnterprises(Model model) {
+        List<tbEnterprise> ViewListEnterprises = this.enterpriseService.listarEmpresas();
         model.addAttribute("ViewListEnterprises", ViewListEnterprises);
         return "ListEnterprise";
-        
->>>>>>> 0fe94f61f35c575821d06e04fe5876b4027ed031
+
     }
 }
